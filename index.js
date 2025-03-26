00b3a9bfc9a4d4e83ec1c1593d5bbf17ee11b9ff
Abhishhek09prajapati <abhishek091119@gmail.com>
2025-03-26 11:20:26 +0530

    sdf

---
+++ index.js
@@ -0,0 +1,7 @@
+var mongoose = require('mongoose');
+
+mongoose.connect("mongodb://127.0.0.1:27017/")
+.then(()=>console.log("Connected to MongoDB"))
+.catch((err) => console.log("Error connecting")
+)
+
