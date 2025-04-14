b09bdb2cab551d795092fff15efa223284193af9
Abhishhek09prajapati <abhishek091119@gmail.com>
2025-04-14 10:07:30 +0530

    seconf

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-var mongoose = require('mongoose');
-
-mongoose.connect("mongodb://127.0.0.1:27017/")
-.then(()=>console.log("Connected to MongoDB"))
-.catch((err) => console.log("Error connecting")
-)
-
+const jsonurl = "https://api.npoint.io/3de7ca257b483136648c";
+const companyname = document.getElementById('companyname');
+let close2 = document.getElementById('menu1');
+fetch(jsonurl)
+.then(data=> data.json())
+.then(data=>{
+    companyname.innerHTML = data[0].Companyname;
+})
+var list1 = document.getElementById("ul1");
+close2.addEventListener("click",function(){
+    if( close2.innerHTML == "menu"){
+        close2.innerHTML = "close";
+        list1.style.display = "block";
+    }else{
+        close2.innerHTML = "menu";
+        list1.style.display = "none";
+    }
+});
