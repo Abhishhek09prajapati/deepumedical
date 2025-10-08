bc20bc8d8d753a70cd246d74ffa77b768036bff5
Abhishhek09prajapati <abhishek091119@gmail.com>
2025-10-08 09:50:10 +0530

    all set

--- calculator/index.js
+++ calculator/index.js
@@ -5,6 +5,7 @@ var outputanswer = document.getElementById('discountans1');
 var customerdiscount1 = document.getElementsByClassName('customerdiscount1')[0];
 var customerdiscountbutton = document.getElementById('customerdiscount');
 var Gstcount = document.getElementById('Gstcount');
+var additmes = document.getElementsByClassName('additmes')[0];
 var gstvalue1 = document.getElementsByClassName('gst')[0];
 var discountans = 0;
 discount.addEventListener('click',()=>{
@@ -34,4 +35,34 @@ Gstcount.addEventListener('click',()=>{
     }else{
          outputanswer.innerHTML = "Don't empty any box";
     }
+})
+var billamout = 0;
+document.getElementById('Add-items').addEventListener('click',()=>{
+    const tablemrp = mrp.value.trim();
+    const tablediscount = customerdiscount1.value.trim();
+    const tableitems = additmes.value.trim();
+
+    if(!tablemrp||!tablediscount||!tableitems){
+        alert("Please Inter Some Value");
+        return;
+    }
+    const table = document.getElementById('itmesofmedicine').querySelector('tbody');
+    const newRow = table.insertRow();
+
+    const customeramount = mrp.value-mrp.value*customerdiscount1.value/100 ;
+
+    billamout+=customeramount;
+
+    newRow.insertCell(0).textContent = tableitems;
+    newRow.insertCell(1).textContent = tablemrp;
+    newRow.insertCell(2).textContent = tablediscount;
+    newRow.insertCell(3).textContent = customeramount;
+
+    mrp.value ='';
+    customerdiscount1.value='';
+    additmes.value='';
+
+    const totalbillamount = document.getElementById('totalbillamount');
+    totalbillamount.innerHTML = ' '+billamout ;
+
 })
