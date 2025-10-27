6412245e0134fa541ba58e27e7a15039d203daa2
Abhishek Prajapati <126238487+Abhishhek09prajapati@users.noreply.github.com>
2025-10-26 21:13:48 -0700

    ddd

--- customerdata/index.js
+++ customerdata/index.js
@@ -3,42 +3,60 @@ var customernumber = document.getElementById('customernumber');
 var userName = document.getElementsByClassName('user')[0];
 var userAddress = document.getElementsByClassName('user')[1];
 var userhistory = document.getElementsByClassName('userhistory')[0];
-
-
-
+var customerdiv = document.getElementsByClassName("customerdiv")[0];
+var customerdivname = document.getElementsByClassName("customerdiv1")[0];
 
 var user = '';
 var usernumber = "";
 var usermedicine = [];
 var userAddress1 = "";
 
-var d = new Date()
-
-function userdeatiles(){
+function userdeatiles() {
     fetch('data.json')
-    .then(res=>res.json())
-    .then(data =>{
-        
-        for(let i=0;i<data.length;i++){
-           if(customernumber.value == data[i].number){
-            user = data[i].name;
-            usernumber = data[i].number;
-            usermedicine = data[i].medicine;
-            userAddress1 = data[i].Address;
-           }
-        }
-        userName.innerHTML = user.toLocaleUpperCase();
-        userAddress.innerHTML = userAddress1.toLocaleUpperCase();
+        .then(res => res.json())
+        .then(data => {
+            // Reset previous data
+            user = '';
+            usernumber = '';
+            usermedicine = [];
+            userAddress1 = '';
 
-        for(let i = 0;i<usermedicine.length;i++){
-            const li = document.createElement('li');
-            li.innerHTML = usermedicine[i];
-            userhistory.appendChild(li)
-        }
+            // Try to find user by number
+            const foundUser = data.find(item => customernumber.value == item.number);
 
-    })
+            if (foundUser) {
+                user = foundUser.name;
+                usernumber = foundUser.number;
+                usermedicine = foundUser.medicine;
+                userAddress1 = foundUser.Address;
+                customerdiv.style.display = "block";
+    
+            } else {
+                alert("Customer not found!");
+                customerdiv.style.display = "none";
+            }
+        })
+        .catch(err => {
+            console.error("Error fetching data:", err);
+            alert("Failed to load data.json");
+        });
 }
 
+function customerdoova() {
+    userName.innerHTML = user.toUpperCase();
+    userAddress.innerHTML = userAddress1.toUpperCase();
+    customerdivname.style.display = "block";
+}
 
+function customerprofile() {
+    // Clear previous list first
+    userhistory.innerHTML = '';
 
-
+    // Create list items for each medicine
+    usermedicine.forEach(med => {
+        const li = document.createElement('li');
+        li.textContent = med;
+        userhistory.appendChild(li);
+    });
+   
+}
